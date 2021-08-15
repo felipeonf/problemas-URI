6655c69adca5454e942920234afd9f7939a0731e
Felipe Oliveira <62366588+felipeonf@users.noreply.github.com>
2021-08-14 22:43:21 -0300

    Create 1007.cpp

---
+++ 1007.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int a, b, c, d, diff;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    diff = (a * b - c * d);
+    cout << "DIFERENCA = " << diff << endl;
+
+return 0;
+}
