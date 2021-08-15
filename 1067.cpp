4af1327b4030e402eefcda5e70370af56875fd74
Felipe Oliveira <62366588+felipeonf@users.noreply.github.com>
2021-08-14 22:26:05 -0300

    Create 1067.cpp

---
+++ 1067.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int numero;
+  cin >> numero;
+
+  for (int aux = 1; aux <= numero; aux = aux + 2)
+  {
+    cout << aux << endl;
+  }
+  
+return 0;
+}
