76d15860f74adcfdae04fce76b0f53d79bdb375b
Felipe Oliveira <62366588+felipeonf@users.noreply.github.com>
2021-08-14 22:18:38 -0300

    Create  1924.cpp

---
+++ 1924.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n_cursos;
+  string curso;
+  cin >> n_cursos;
+  for (int aux = 1; aux <= n_cursos; aux++)
+  {
+    cin >> curso;
+  }
+  cout << "Ciencia da Computacao" << endl;
+
+return 0;
+}
