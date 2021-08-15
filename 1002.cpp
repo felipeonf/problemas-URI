0f8bfe0c9111c12575515693e7e6e399e3c6e911
Felipe Oliveira <62366588+felipeonf@users.noreply.github.com>
2021-08-14 22:45:30 -0300

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    double r, PI, area;
+    PI = 3.14159;
+    cin >> r;
+    area = 3.14159 * r * r;
+    printf("A=""%.4f""\n", area);
+    
+ 
+return 0;
+}
